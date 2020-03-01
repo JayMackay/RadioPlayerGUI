4b541a7f803f6bc6ecdfb46db9ab4b414d4e5a3f
JayMackay <jared.mackay1994@live.com>
2020-03-01 10:59:02 +0000

    Material Design Card Animation & Channel View

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +18,63 @@ using System.Windows.Shapes;
 
 namespace RadioApplication
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged 
     {
+        //OBJECT INITIALIZATION
+        private ObservableCollection<ChannelModel> currentChannel = new ObservableCollection<ChannelModel>();
+        Radio radio = new Radio();
+
         public MainWindow()
         {
             InitializeComponent();
+            CurrentChannel.Add(new ChannelModel() { Brush = Brushes.Gray, ChannelName = "Channel One" });
+            CurrentChannel.Add(new ChannelModel() { Brush = Brushes.CornflowerBlue, ChannelName = "Channel Two" });
+            CurrentChannel.Add(new ChannelModel() { Brush = Brushes.CadetBlue, ChannelName = "Channel Three" });
+            CurrentChannel.Add(new ChannelModel() { Brush = Brushes.LightBlue, ChannelName = "Channel Four" });
+        }
+
+        //CHANNEL COLLECTION
+        public ObservableCollection<ChannelModel> CurrentChannel 
+        { 
+            get { return currentChannel; }
+            set
+            {
+                currentChannel = value;
+                NotifyPropertyChanged("CurrentChannel");
+            }
+        }
+
+        //CHANNEL EVENT HANDLER
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        //CHANNEL ONE (BBC RADIO ONE)
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            Player.Source = new Uri("http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p", UriKind.RelativeOrAbsolute);
+            Player.Play();
+        }
+
+        private void PowerButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SkipPrevious_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SkipNext_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Radio.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioApplication
+{
+    [Serializable]
+    class Radio
+    {
+        //FIELDS
+        private int _channel = 1;
+        private bool _on = false;
+        private int _volume = 0;
+
+        //CHANNEL PROPERTY
+        public int Channel
+        {
+            get
+            {
+                return _channel;
+            }
+
+            set
+            {
+                if (value <= 4 && value > 0 && _on == true)
+                {
+                    _channel = value;
+                }
+            }
+        }
+
+        //VOLUME PROPERTY
+        public int Volume
+        {
+            get { return _volume; }
+            set
+            {
+                if (value >= 0 && value <= 30 && _on == true)
+                {
+                    _volume = value;
+                }
+            }
+        }
+
+        //POWER ON METHOD
+        public void TurnOn()
+        {
+            _on = true;
+            //return "Radio is on";
+        }
+
+        //POWER OFF METHOD
+        public string TurnOff()
+        {
+            _on = false;
+            return "Radio is off";
+        }
+
+
+        //PLAY CHANNEL METHOD
+        public string Play()
+        {
+            int channelNumber = _channel;
+
+            if (_on == true)
+            {
+                return $"Playing channel {channelNumber}";
+            }
+            else
+            {
+                return "Radio is off";
+            }
+
+        }
+
+        //VOLUME CONTROL
+        public int volumeControl()
+        {
+            return _volume;
+        }
+
+
+        /*//SERIALIZE
+        public void Write()
+        {
+            _channel = Channel;
+            _volume = Volume;
+
+            string output = JsonConvert.SerializeObject(this);
+            File.WriteAllText(path, output);
+        }
+
+        //DESERIALIZE
+        public void Read()
+        {
+            string jsonfile = File.ReadAllText(path);
+            var info = JsonConvert.DeserializeObject<Radio>(jsonfile);
+
+        }*/
+    }
+}
