207d421b13c6308d8bae5476e45dca811c43b041
JayMackay <jared.mackay1994@live.com>
2020-03-01 10:58:03 +0000

    Channel Model & Collection Implementation

---
+++ ChannelModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Media;
+
+namespace RadioApplication
+{
+    public class ChannelModel
+    {
+        //FIELDS
+        private string channelName = "";
+        private Brush brush = Brushes.Transparent;
+
+        //PROPERTIES
+        public string ChannelName { get => channelName; set => channelName = value; }
+        public Brush Brush { get => brush; set => brush = value; }
+
+        //CHANNEL MODEL CONSTRUCTOR
+        public ChannelModel()
+        {
+        }
+    }
+}
